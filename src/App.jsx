045a286df626b64a8679b34f924b9d5fca539815
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-10 21:12:34 +0100

    took care of warnings

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import * as ACTIONS from "./store/actions/actions";
 import Container from './components/Container'
 
-import Loader from './components/Loader';
+// import Loader from './components/Loader';
 
 class App extends Component {
   constructor(props) {
